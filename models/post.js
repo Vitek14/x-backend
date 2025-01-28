4f56d553464eafb55e490e75a4c23183e3a02192
Vitek14 <yudin.vitovt@gmail.com>
2025-01-28 22:48:51 +0300

    Updated Posts Model according to Miro mapping Added beforeCreate hook to set parent_post_id = post.id, if not provided(test)

--- models/post.js
+++ models/post.js
@@ -15,32 +15,51 @@ const Post = sequelize.define('post', {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  image: {
+  image_url: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  date: {
+  created_at: {
     type: DataTypes.DATE,
     allowNull: false,
   },
   reposts_count: {
     type: DataTypes.INTEGER,
     allowNull: false,
-  },
-  views_count: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  likes_count: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
+    defaultValue: 0,
   },
   comments_count: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    defaultValue: 0,
   },
+  views_count: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  likes_count: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  parent_post_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  bookmarked: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  }
 }, {
   timestamps: false,
+});
+
+// Хук для установки parent_post_id равным id
+Post.beforeCreate((post) => {
+  if (!post.parent_post_id) {
+    post.parent_post_id = post.id; // Устанавливаем parent_post_id равным id
+  }
 });
 
 module.exports = Post;
