15660477ddee00339514acd6d1eb61212a4333a5
Vitek14 <yudin.vitovt@gmail.com>
2025-02-03 18:11:18 +0300

    Added sequelize-cli seed and cfg files

---
+++ seeders/20250203144218-seed-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const User = require("../models/user");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return await queryInterface.bulkInsert("users", [
+      {
+      first_name: 'John',
+      last_name: "Doe",
+      user_name: "john_doe",
+      joined_date: "24-02-2025",
+      website: "johndoe.com",
+      description: "Johndoe.com",
+      avatar_url: "https://www.deepseek.com/favicon.ico",
+      banner_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/DeepSeek_logo.svg/1024px-DeepSeek_logo.svg.png",
+      workplace: "deepseek",
+      location: "Earth",
+      birthdate: "20-02-1999",
+      email: 'john@example.com',
+      password: await User.generatePassword("1234")
+    }
+    ]);
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("users", null, {});
+  }
+};
