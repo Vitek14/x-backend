bfb9c120414cdc2b1f8ad8097bcb99eaa39b525f
Vitek14 <yudin.vitovt@gmail.com>
2025-02-13 19:35:16 +0300

    Implemented authentication middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const privateKey = process.env.PRIVATE_KEY;
+
+exports.verifyToken = async (req, res, next) => {
+  const token = req.headers['x-auth-token'];
+  if (!token) {
+    return res.status(403).json({ error: 'No token provided' });
+  }
+  try {
+    const decoded = jwt.verify(token, privateKey);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+// module.exports = verifyToken;
